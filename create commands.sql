2c7b7a64042a27611b4e370ae47d9b4243b795de
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2025-07-21 16:00:31 +0530

    Day 1

---
+++ create commands.sql
@@ -0,0 +1,22 @@
+-- create database 
+-- use firstdatabase
+
+-- create table
+-- CREATE TABLE Emp (
+--   id INT,
+--   name VARCHAR(50),
+--   sal INT,
+--   dpt_id INT,
+--   PRIMARY KEY (id)
+-- );
+
+select * from Emp;
+
+
+-- for adding the data in the table
+
+-- INSERT INTO Emp (id,name,sal,dpt_id)
+--  values(1,'Kush',4000,30),
+--         (2,'Hussar',5000,40),
+--         (3,'kushr',5000,40),
+--         (4,'piyush',400,40);
